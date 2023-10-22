7156071146a6a63122d0f2bc5ec3cb58c274dba8
Ante-237 <akumawahanye@gmail.com>
2023-10-22 18:28:25 +0200

    Definations
    Defining the methods needed.

--- Timer/Timer.cs
+++ Timer/Timer.cs
@@ -1,11 +1,46 @@
 ﻿using System;
+using System.Timers;
+
+
+
 
 namespace StopWatch{
 
     public class Timer{
 
+        private static Timer _time;
+
         public static void Main(){
-            Console.WriteLine("shoes");
+           Entry();
+
+        }
+
+        public static void Entry(){
+            _time = new Timer();
+            
+             Console.WriteLine("\n\n\t\t\tSTOPWATCH CONTROLS\n\n [S] == START  |  [P] == PAUSE  |  [R] == RESUME  |  [Z] == RESET  |  [F] == STOP");
+             Console.WriteLine("\n\t\t\tCURRENT TIME : {0:HH:mm:ss}", DateTime.Now);
+        }
+
+
+        public static void StartButton(){
+                // S to start
+        }
+
+        public static void PauseButton(){
+                // P to pause
+        }
+
+        public static void ResumeButton(){
+                // R to resume
+        }
+
+        public static void ResetButton(){
+                // Z to reset
+        }
+
+        public static void StopButton(){
+                // F to stop
         }
 
     }
