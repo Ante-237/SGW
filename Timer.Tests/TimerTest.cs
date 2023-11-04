ac08f0de23c8ef8c92c5816d607f2a4bc3157092
aluwaye-noah <a.yusuf1@gmail.com>
2023-11-04 06:33:04 -0700

    timer

--- Timer.Tests/TimerTest.cs
+++ Timer.Tests/TimerTest.cs
@@ -3,33 +3,35 @@ using WatchStuff;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+	    [SetUp]
+	        public void Setup()
+			    {
+				        }
 
-    [Test]
-    public void TestStartButton()
-    {
-        Watch.StartButton();
-        Assert.IsTrue(Watch.isRunning);
-    }
+	        [Test]
+		    public void TestStartButton()
+			        {
+					        Watch.StartButton();
+						        Assert.IsTrue(Watch.isRunning);
+							    }
 
-    [Test]
-    public void TestPauseButton(){
-        Watch.PauseButton();
-        Assert.IsFalse(Watch.isRunning);
-    }
+		    [Test]
+		        public void TestPauseButton(){
+				        Watch.PauseButton();
+					        Assert.IsFalse(Watch.isRunning);
+						    }
 
-    [Test]
-    public void TestResumeButton(){
-        Watch.ResumeButton();
-        Assert.IsTrue(Watch.isRunning);
-    }
+		        [Test]
+			    public void TestResumeButton(){
+				            Watch.ResumeButton();
+					            Assert.IsTrue(Watch.isRunning);
+						        }
 
-    [Test]
-    public void TestResetButton(){
-        Watch.ResetButton();
-        Assert.IsFalse(Watch.isRunning);
-    {
+			    [Test]
+			        public void TestResetButton(){
+					        Watch.ResetButton();
+						        Assert.IsFalse(Watch.isRunning);
+							    }
+
+
 }
